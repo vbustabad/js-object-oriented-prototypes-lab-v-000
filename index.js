e4e3364739a488acbc794c44ef3d79d68d0ace67
Veronica Bustabad <vbustabad@gmail.com>
2018-09-09 20:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ function BoardMember(name, homeState, training){
   this.training = training;
 }
 
-  //   this.veto = function(){
-  //     return 'No, I must disagree'
-  //   }
-  //   this.approve = function(){
-  //     return 'You can do that!'
-  //   }
-  //   this.doCharity = function(){
-  //     return "I like to help people."
-  //   }
-  //
-  //   this.releasePressStatement = function(){
-  //     return "You will see great things from Scuber."
-  //   }
-  //
-  //   this.sayHi = function(){
-  //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  //   }
-  // }
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
+}
+
+    this.approve = function(){
+      return 'You can do that!'
+    }
+    this.doCharity = function(){
+      return "I like to help people."
+    }
+  
+    this.releasePressStatement = function(){
+      return "You will see great things from Scuber."
+    }
+  
+    this.sayHi = function(){
+      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    }
+  }
