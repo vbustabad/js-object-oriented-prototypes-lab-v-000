8001b56cb918b8984fc6e1ce5925491a7b944df5
Veronica Bustabad <vbustabad@gmail.com>
2018-09-09 20:36:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ BoardMember.prototype.releasePressStatement = function(){
   return "You will see great things from Scuber.";
 }
 
-  //   this.sayHi = function(){
-  //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-  //   }
-  // }
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+}
